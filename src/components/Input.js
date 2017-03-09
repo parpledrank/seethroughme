8d8a29be2671c57828d268819ed34020ee16279c
j-s-o <jso.jonathan@gmail.com>
2017-03-09 15:12:30 -0800

    Basic Routing Set

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class Input extends Component {
+  render() {
+    return (
+      <div>Input</div>
+    );
+  }
+}
+
+export default Input;
