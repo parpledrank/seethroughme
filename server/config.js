cb17ba1f5feaf04dd504eb03faa82c1c45d44c94
j-s-o <jso.jonathan@gmail.com>
2017-03-13 20:41:48 -0700

    fix

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
-  API_KEY_TRANSLATE: 'AIzaSyBypkxTWb6LG2_N7OOnF_C1_j5OyBPHJmA',
-  API_KEY_VR: '3936275c1ba6b366130abce63493b2250a8f24f1'
+  //will include when running app in production server. removed for github security reasons - jso
+  API_KEY_TRANSLATE: '',
+  API_KEY_VR: ''
 }
--- server/server.js
+++ server/server.js
@@ -14,9 +14,13 @@ app.use(jsonParser);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('bundles', express.static(path.join(__dirname, '../bundles')));
+app.use('/bundles', express.static(path.join(__dirname, '../bundles')));
 
 app.use('/api', apiRouter);
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+})
 
 app.listen(PORT, () => {
   console.log(`App is listening at port ${PORT}.`)
