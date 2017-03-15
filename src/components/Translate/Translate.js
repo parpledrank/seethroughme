c3b948fe4ac467c85985f611d97dff2782fff773
j-s-o <jso.jonathan@gmail.com>
2017-03-14 19:11:42 -0700

    Adjusted routing so that the translate component will have access to the keywords returned by the IPM api.

--- src/components/Translate/Translate.js
+++ src/components/Translate/Translate.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 //import '../../styles/App.css';
 
 class Translate extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log(this.props);
+  }
   render() {
     return (
       <div>Translate</div>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { IndexRoute, Router, Route, Link, browserHistory } from 'react-router';
 
@@ -8,9 +8,38 @@ import Translate from './components/Translate/Translate.js'
 import './styles/index.css';
 import App from './components/app.jsx'
 
+class Root extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      keywords: []
+    }
+
+    this.setRootKeywords = this.setRootKeywords.bind(this);
+  }
+
+  setRootKeywords(keywords) {
+    this.setState({ keywords }, () => {
+      browserHistory.push('/translate');
+    })
+  }
+
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div>{ children && React.cloneElement(children, { setRootKeywords: this.setRootKeywords, keywords: this.state.keywords })}</div>
+    )
+  }
+}
+
 ReactDOM.render((
   <Router history={browserHistory}>
-    <Route path='/' component={App} />
-    <Route path='/translate' component={Translate} />
+    <Route path='/' component={Root}>
+      <IndexRoute component={App} />
+      <Route path='/translate' component={Translate}>
+      </Route>
+    </Route>
   </Router>
 ), document.getElementById('container'));
