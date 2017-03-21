260cea523acda45543a006dcc8fb154af9e39e69
root <root@seethroughme.localdomain>
2017-03-21 21:57:38 +0000

    server commits

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: process.env.PORT || 8080,
+  PORT: 80,
   API_KEY_TRANSLATE: 'AIzaSyBEb2nG4J6FMbY-3cmXBWL9nCGWp-fsx78',
   API_KEY_VR: 'f45de3b0f3b6dda19ddc38820298804b9a313fb2'
 }
